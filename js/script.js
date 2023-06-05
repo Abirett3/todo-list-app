837022047914d0821a0b72c98c69260ebdc13755
Ashfaque Hossain Abir <108189537+Abirett3@users.noreply.github.com>
2023-06-05 12:56:17 +0545

    Files added

---
+++ js/script.js
@@ -0,0 +1,229 @@
+/* ==================================
+-- copyright 2023 A.H. Abir
+===================================== */
+
+const taskInput = document.querySelector(".task-input input"),
+filters = document.querySelectorAll(".filters span"),
+clearAll = document.querySelector(".clear-btn"),
+taskBox = document.querySelector(".task-box");
+
+// Import task complete sound
+const taskCompleteSound = new Audio("./sounds/task-complete.mp3");
+
+// --------------------------------------------------------------------------
+// => Time settings
+
+// creating a function and calling it in every seconds
+setInterval(()=>{
+
+  let date = new Date(),
+  hour = date.getHours(),
+  min = date.getMinutes(),
+  sec = date.getSeconds();
+
+  let d;
+  d = hour < 12 ? "AM" : "PM"; //if hour is smaller than 12, than its value will be AM else its value will be pm
+  hour = hour > 12 ? hour - 12 : hour; //if hour value is greater than 12 than 12 will subtracted ( by doing this we will get value till 12 not 13,14 or 24 )
+  hour = hour == 0 ? hour = 12 : hour; // if hour value is  0 than it value will be 12
+
+  // adding 0 to the front of all the value if they will less than 10
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  document.querySelector(".hour_num").innerText = hour;
+  document.querySelector(".min_num").innerText = min;
+  // document.querySelector(".sec_num").innerText = sec;
+  document.querySelector(".am_pm").innerText = d;
+
+}, 1000); // 1000 milliseconds = 1s
+// --------------------------------------------------------------------------
+
+// Import Header Time
+const headerTime = document.querySelector("[data-header-time]");
+
+// store current date from build-in date object
+const date = new Date();
+
+
+
+// --------------------------------------------------------------------------
+/**
+ * convert weekday number to weekday name
+ * totalParameter: 1;
+ * parameterValue: <number> 0-6;
+ */
+
+const getWeekDayName = function (dayNumber) {
+    switch (dayNumber) {
+      case 0:
+        return "Sunday";
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "Wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+      default:
+        return "Not a valid day";
+    }
+  }
+  
+  
+  
+  /**
+   * convert month number to month name
+   * totalParameter: 1;
+   * parameterValue: <number> 0-11;
+   */
+  
+  const getMonthName = function (monthNumber) {
+    switch (monthNumber) {
+      case 0:
+        return "Jan";
+      case 1:
+        return "Feb";
+      case 2:
+        return "Mar";
+      case 3:
+        return "Apr";
+      case 4:
+        return "May";
+      case 5:
+        return "Jun";
+      case 6:
+        return "Jul";
+      case 7:
+        return "Aug";
+      case 8:
+        return "Sep";
+      case 9:
+        return "Oct";
+      case 10:
+        return "Nov";
+      case 11:
+        return "Dec";
+      default:
+        return "Not a valid month";
+    }
+  }
+  
+  
+  
+  // store weekday name, month name & month-of-day number
+  const weekDayName = getWeekDayName(date.getDay());
+  const monthName = getMonthName(date.getMonth());
+  const monthOfDay = date.getDate();
+  
+  // update headerTime date
+  headerTime.textContent = `${weekDayName}, ${monthName} ${monthOfDay}`;
+// --------------------------------------------------------------------------
+
+let editId,
+isEditTask = false,
+todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });
+});
+
+function showTodo(filter) {
+    let liTag = "";
+    if(todos) {
+        todos.forEach((todo, id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if(filter == todo.status || filter == "all") {
+                liTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" style="cursor: pointer;" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }
+        });
+    }
+    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+}
+showTodo("all");
+
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != selectedTask) {
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+        taskCompleteSound.play(); // Here the sound can be insert -------------------------------------------
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos))
+}
+
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+
+function deleteTask(deleteId, filter) {
+    isEditTask = false;
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(filter);
+}
+
+clearAll.addEventListener("click", () => {
+    isEditTask = false;
+    todos.splice(0, todos.length);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo()
+});
+
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask) {
+        if(!isEditTask) {
+            todos = !todos ? [] : todos;
+            let taskInfo = {name: userTask, status: "pending"};
+            todos.push(taskInfo);
+        } else {
+            isEditTask = false;
+            todos[editId].name = userTask;
+        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo(document.querySelector("span.active").id);
+    }
+});
